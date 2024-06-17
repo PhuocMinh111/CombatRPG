bd283c93a783e12ee9790e88c223852692bc718f
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-06-17 21:25:17 +0700

    reset to idle state when finish one attack

--- Assets/Scripts/Player/States/Attack/Attack.cs
+++ Assets/Scripts/Player/States/Attack/Attack.cs
@@ -1,91 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AttackState : PlayerState
 {
-    private int comboClick = 0;
-    private int attacksNumber;
-    private bool clickedInframe;
+
+    private int comboCounter = 0;
+    private int comboNumber;
     private float lastTimeAttack;
     private float comboWindow = 1f;
     public AttackState(Player _player, PlayerStateMachine _playerStateMachine, string _animBoolName) : base(_player, _playerStateMachine, _animBoolName)
     {
-        PlayerState Attack1 = new(_player, _playerStateMachine, GetAnim(Anim.Attack1));
-        PlayerState Attack2 = new(_player, _playerStateMachine, GetAnim(Anim.Attack2));
-        PlayerState Attack3 = new(_player, _playerStateMachine, GetAnim(Anim.Attack3));
-        AddSubState(Attack1);
-        AddSubState(Attack2);
-        attacksNumber = SubStates.Count;
-        this.CurrentSubState = Attack1;
+
+
+        comboNumber = 1;
+
     }
     public override void Enter()
     {
         base.Enter();
+        if (comboCounter > comboCounter || Time.time >= lastTimeAttack + comboWindow)
+            comboCounter = 0;
 
-        playerAnimator.SetInteger("ComboCounter", comboClick);
-        clickedInframe = false;
+        playerAnimator.SetInteger("Combo", comboCounter);
+
+    }
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        Debug.Log("combo counter " + comboCounter);
+        // Debug.Log("combo counter " + comboCounter);
+
     }
 
     public override void Exit()
     {
         base.Exit();
 
-
-
         lastTimeAttack = Time.time;
-        Debug.Log("lastTimeAttack " + lastTimeAttack);
-    }
-    public override void FixedUpdate()
-    {
-        base.FixedUpdate();
-        Debug.Log("combo counter " + comboClick);
+        comboCounter++;
+
+        Debug.Log("last time attack" + lastTimeAttack);
+        Debug.Log("combo counter " + comboCounter);
+
+        // Debug.Break();
 
     }
+
     public override void Update()
     {
         base.Update();
 
-
-
         if (animationTrigger)
-        {
-            if (comboClick > 0)
-            {
-                if (Time.time > lastTimeAttack + comboWindow)
-                    comboClick = 0;
+            stateMachine.ChangeState(stateMachine.IdleState);
 
-                playerAnimator.SetInteger("ComboCounter", comboClick);
-            }
-            else
-            {
-
-                if (xInput == 0)
-                    ChangeState(stateMachine.IdleState);
-                else
-                    ChangeState(stateMachine.MoveState);
-            }
-        }
-        else
-        {
-            if (Input.GetMouseButton(0))
-            {
-                Debug.Log("combo clicked ");
-                if (!clickedInframe)
-                {
-                    comboClick++;
-                    clickedInframe = true;
-                }
-            }
-        }
-
-    }
-    public void Attack()
-    {
-        if (comboClick > attacksNumber)
-            comboClick = 0;
-
-        playerAnimator.SetInteger("ComboCounter", comboClick);
     }
 
 }
